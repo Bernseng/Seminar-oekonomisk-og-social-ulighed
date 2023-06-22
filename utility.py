e183e5be49ae0867449a6ae5027169b96d24f34c
Bernseng <kjp538@alumni.ku.dk>
2023-06-22 13:34:33 +0200

    push

--- utility.py
+++
@@ -1,9 +0,0 @@
-from numba import njit
-
-@njit(parallel=True)
-def func(c,l,par):
-    return (c**(1-par.sigma)/(1-par.sigma))-par.varphi*(l**(1+par.nu)/(1+par.nu))
-
-# for 3d plot
-def func_2(c,l,par):
-    return (c**(1-par.sigma)/(1-par.sigma))-par.varphi*(l**(1+par.nu)/(1+par.nu))
